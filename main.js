1ae2e121a46b1120bd17ada10452a1ae67cd6d48
jasenmichael <jasen@jasenmichael.com>
2022-03-04 15:32:55 -0600

    chore: implement hide free items

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-// (() => {
-//   console.log("Hello World");
-//   const products = document.getElementsByClassName("product");
-//   console.log(products);
-//   for (const product of products) {
-//     console.log(product);
-//   }
-// })();
-
-
 window.addEventListener("load", function() {
-    // alert('Page is loaded');
-    console.log("Hello World");
     const products = [...document.getElementsByClassName("product")]
     products.forEach((product, i) => {
         console.log(product.getElementsByClassName("product__price")[0].innerText.trim());
@@ -19,6 +7,5 @@ window.addEventListener("load", function() {
         if (!isFree) {
             product.classList.add("visible");
         }
-        // console.log(JSON.stringify(product.innerText), i);
     })
 });
