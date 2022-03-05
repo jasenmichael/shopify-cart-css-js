14f7127170f8a6a2afce8276f0cc9918ce6e6205
jasenmichael <jasen@jasenmichael.com>
2022-03-05 09:40:58 -0600

    chore: add os chosen

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ window.addEventListener("load", function() {
     products.forEach((product, i) => {
         console.log(product.getElementsByClassName("product__price")[0].innerText.trim());
         const isFree = product.getElementsByClassName("product__price")[0].innerText.trim() === "Free";
-        console.log(i, 'isFree:', isFree);
-        if (!isFree) {
+        const titleWhitelist = [
+            "Pop!_OS",
+            "Ubuntu"
+        ]
+        const title = product.getElementsByClassName("product__description__name")[0].innerText.trim().split("\n")[0];
+        const isWhitelist = titleWhitelist.some(t => title.includes(t));
+        if (!isFree || isWhitelist) {
             product.classList.add("visible");
         }
     })
