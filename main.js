a05c4fb48e1a16124a9407a5b450a126516044c3
jasenmichael <jasen@jasenmichael.com>
2022-03-05 11:18:11 -0600

    chore: add loading spinner

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 window.addEventListener("load", () => {
-    const products = [...document.getElementsByClassName("product")]
-    products.forEach((product, i) => {
-        const isFree = product.getElementsByClassName("product__price")[0].innerText.trim() === "Free";
-        const titleWhitelist = [
-            "Pop!_OS",
-            "Ubuntu"
-        ]
-        const title = product.getElementsByClassName("product__description__name")[0].innerText.trim().split("\n")[0];
-        const isWhitelist = titleWhitelist.some(t => title.includes(t));
-        const spinner = document.querySelector('#spinner')
-        if (spinner) {
-            spinner.parentNode.removeChild(spinner);
-        }
-        if (!isFree || isWhitelist) {
-            product.classList.add("visible");
-        }
-    })
+  const list = document.querySelector(
+    '[data-order-summary-section="line-items"]'
+  );
+  const spinnerTemplate = `
+  <tr id="spinner">
+    <td>
+      <img src="https://jasenmichael.github.io/shopify-checkout-css-js/spinning-loading.gif" />
+    </td>
+  </tr>`;
+  list.innerHTML = spinnerTemplate + list.innerHTML;
+
+  const products = [...document.getElementsByClassName("product")];
+  products.forEach((product, i) => {
+    const isFree =
+      product.getElementsByClassName("product__price")[0].innerText.trim() ===
+      "Free";
+    const titleWhitelist = ["Pop!_OS", "Ubuntu"];
+    const title = product
+      .getElementsByClassName("product__description__name")[0]
+      .innerText.trim()
+      .split("\n")[0];
+    const isWhitelist = titleWhitelist.some((t) => title.includes(t));
+    const spinner = document.querySelector('#spinner')
+    if (spinner) {
+        spinner.parentNode.removeChild(spinner);
+    }
+    if (!isFree || isWhitelist) {
+      product.classList.add("visible");
+    }
+  });
 });
